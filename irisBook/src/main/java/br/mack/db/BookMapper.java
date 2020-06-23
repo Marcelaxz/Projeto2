d9af588e46b859d5a5aadc0005216a8f8f5b1f5f
bii-mm <61274369+bii-mm@users.noreply.github.com>
2020-06-23 17:27:49 -0300

    BookMapper
    Criação do BookMapper

---
+++ irisBook/src/main/java/br/mack/db/BookMapper.java
@@ -0,0 +1,20 @@
+package br.mack.db;
+
+import br.mack.api.Book;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookMapper {
+
+    @Override
+    public Book map(ResultSet rs, StatementContext ctx) throws SQLException {
+        return new Book(
+                rs.getInt("id"),
+                rs.getInt("qtd"),
+                rs.getDate("week")
+        );
+    }
+}

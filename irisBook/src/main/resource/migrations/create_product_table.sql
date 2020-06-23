b0c16568c2ff73880c74f186a2538382f054c0e2
Marcelaxz <marcela590@outlook.com>
2020-06-23 18:12:50 -0300

    :O

--- irisBook/src/main/resource/migrations/create_product_table.sql
+++ irisBook/src/main/resource/migrations/create_product_table.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
-      create table product (
+      create table book (
           id int not null auto_increment primary key,
           qtd int not null,
           week DATE not null

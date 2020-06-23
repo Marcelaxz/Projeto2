000eefe67c4c6b03577a56f5077588cb8f749002
Marcelaxz <marcela590@outlook.com>
2020-06-23 17:21:13 -0300

    BookDao/BookResource

---
+++ irisBook/src/main/java/br/mack/db/BookDao.java
@@ -0,0 +1,27 @@
+package br.mack.db;
+
+import br.mack.api.Book;
+import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.util.List;
+@RegisterRowMapper(ProductMapper.class)
+
+public interface BookDao {
+        @SqlUpdate("insert into book (qtd, week) values (:qtd, :week)")
+        @GetGeneratedKeys
+        long insert(@BindBean Book book);
+
+        @SqlQuery("select * from book")
+        List<Book> getAllBooks();
+
+        @SqlQuery("select * from book where id = :id")
+        Book findById(@Bind("id") long id);
+
+        @SqlUpdate("delete from book where id = :id")
+        void delete(@Bind("id") long bookId);
+}

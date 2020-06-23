7e56fe107fe6584caea4012fb735799c46f23c84
bii-mm <61274369+bii-mm@users.noreply.github.com>
2020-06-23 17:32:58 -0300

    Update BookMapper
    acho que agora vai

--- irisBook/src/main/java/br/mack/db/BookMapper.java
+++ irisBook/src/main/java/br/mack/db/BookMapper.java
@@ -7,7 +7,7 @@ import org.jdbi.v3.core.statement.StatementContext;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class BookMapper {
+public class BookMapper implements RowMapper<Book>{
 
     @Override
     public Book map(ResultSet rs, StatementContext ctx) throws SQLException {

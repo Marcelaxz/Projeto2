73896b4c3635cb0541f5301fdaec1eab221bd272
bii-mm <61274369+bii-mm@users.noreply.github.com>
2020-06-24 10:13:12 -0300

    Update TracerDAO

--- irisTracer/src/main/java/br/mack/persistencia/TracerDAO.java
+++ irisTracer/src/main/java/br/mack/persistencia/TracerDAO.java
@@ -1,4 +1,10 @@
 package br.mack.persistencia;
+import br.mack.entidade.Tracer;
 
 public interface TracerDAO {
+    boolean create(Tracer tracer);
+    boolean delete(int opc);
+    boolean update(Tracer tracer);
+    void read();
 }
+
